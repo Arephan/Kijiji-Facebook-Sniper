2bcd52cf330ee27f3ca5a51c37afb99ce5bf1c4f
han kim <hankim@hans-MacBook-Air.local>
2019-10-08 18:47:43 -0400

    Fix scrape from desktop view to mobile view
    They lock you out of desktop scrape after 30 or so tries.
    Spent a long time finding class names for selectors

--- scraper.js
+++ scraper.js
@@ -1,32 +1,35 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeFacebook(city, query, maxPrice) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(
-    `https://www.facebook.com/marketplace/${city}/search/?query=${query}&maxPrice=${maxPrice}`
+    `https://mobile.facebook.com/marketplace/${city}/search/?query=${query}&maxPrice=${maxPrice}`
   );
   await autoScroll(page);
 
   let titles = await page.evaluate(() =>
     Array.from(
-      document.querySelectorAll('[data-testid="marketplace_pdp_title"]')
+      document.querySelectorAll('[class="_59k _2rgt _1j-f _2rgt _3zi4 _2rgt _1j-f _2rgt"]')
     ).map(title => title.innerText.replace(/’|'/g, ''))
   );
 
   let prices = await page.evaluate(() =>
     Array.from(
       document.querySelectorAll(
-        '[data-testid="marketplace_feed_item"] div div:first-child div div'
+        '[class="_59k _2rgt _1j-f _2rgt"]'
       )
     ).map(price => price.innerText.replace(/\$/g, ''))
   );
 
-  let urls = await page.evaluate(() =>
+  let urls = await page.evaluate(() => 
     Array.from(
-      document.querySelectorAll('[data-testid="marketplace_feed_item"]')
+      document.querySelectorAll('[class="_9_7 _643_"]')
     ).map(item => item.href)
-  );
+  )
+
+  prices = prices.slice(3,35)
+  urls = urls.slice(2,34)
 
   // @TODO: get picture URLs and add onto 'items'
   // @TODO: set up polling
@@ -68,7 +71,7 @@ async function prepareItems(titles, prices, urls) {
 
     items.push(item);
   }
-  
+
   return items;
 }
 
